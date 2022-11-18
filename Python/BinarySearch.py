0980f1f97454a69b08752d4ab53bb900306672a2
Chintamani Chavan <chintamani.chavan07@gmail.com>
2022-11-18 00:28:10 -0600

    Added and modified longest common subsequence algorithm

--- Python/BinarySearch.py
+++ Python/BinarySearch.py
@@ -1,13 +1,12 @@
-from typing import list
 class BinarySearch(object):
 
-    def __init__ (self, nums: List[int], target: int):
+    def __init__(self, nums: List[int], target: int):
         self.nums = nums
         self.target = target
 
     def binary_search(self, nums: List[int], target: int) -> int:
         begin_index = 0
-        end_index = len(nums) -1
+        end_index = len(nums) - 1
         while begin_index <= end_index:
             mid_index = (end_index + begin_index) // 2
             mid_index_value = nums[mid_index]
@@ -19,15 +18,13 @@ class BinarySearch(object):
 
             else:
                 begin_index = mid_index + 1
-        
+
         return -1
-    
-nums = [-2,3,4,7,8,9,11,13]
+
+
+nums = [-2, 3, 4, 7, 8, 9, 11, 13]
 target = 11
 
-b - BinarySearch()
+b = BinarySearch()
 
-b.binary_search(nums,target)
-
-
-    
+b.binary_search(nums, target)
