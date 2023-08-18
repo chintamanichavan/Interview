ee7a84a4a4935a3832339fe9d6e312deb3aec099
chintamanichavan <chintamani.chavan08@gmail.com>
2023-08-17 19:09:26 -0500

    Single Number II algorithm added

---
+++ Python/SingleNumberII.py
@@ -0,0 +1,15 @@
+class Solution:
+  def singleNumber(self, nums: List[int]) -> int:
+
+        ones, twos = 0, 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
+  
+def main():
+    nums = [0,1,0,1,0,1,99]
+    s = Solution()
+    res = s.singleNumber(nums)
+    print(res)
+
