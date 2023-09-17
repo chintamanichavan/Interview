e9a07d5f405584a4a52095d0e51c72ebd63a63f1
chintamanichavan <chintamani.chavan08@gmail.com>
2023-09-16 20:17:14 -0500

    ShortestPathVisitingAllNodes.py Updated

--- Python/ShortestPathVisitingAllNodes.py
+++ Python/ShortestPathVisitingAllNodes.py
@@ -1,30 +1,29 @@
+from typing import List
+from collections import deque
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
-        
         n = len(graph)
-        
-        # Initialize the dp array
-        dp = [[float('inf')] * (1 << n) for _ in range(n)]
-        
-        # Initialize the starting node's distance
-        for i in range(n):
-            dp[i][1 << i] = 0
-        
-        # Iterate through all possible combinations of visited nodes
-        for mask in range(1 << n):
-            for u in range(n):
-                if mask & (1 << u):
-                    for v in graph[u]:
-                        dp[u][mask] = min(dp[u][mask], dp[v][mask ^ (1 << u)] + 1)
-        
-        # Find the minimum distance to visit all nodes starting and ending at node 0
-        min_distance = min(dp[i][(1 << n) - 1] for i in range(n))
-        
-        return min_distance
+        queue = deque([(1 << i, i, 0) for i in range(n)])
+        visited = set((1 << i, i) for i in range(n))
 
-# Example usage:
-graph1 = [[1, 2, 3], [0], [0], [0]]
-print(shortestPathLength(graph1))  # Output: 4
+        while queue:
+            mask, node, dist = queue.popleft()
+            if mask == (1 << n) - 1:
+                return dist
+            for neighbor in graph[node]:
+                new_mask = mask | (1 << neighbor)
+                if (new_mask, neighbor) not in visited:
+                    visited.add((new_mask, neighbor))
+                    queue.append((new_mask, neighbor, dist + 1))
 
-graph2 = [[1], [0, 2, 4], [1, 3, 4], [2], [1, 2]]
-print(shortestPathLength(graph2))  # Output: 4
+def main():
+    s = Solution()
+    # Example usage:
+    graph1 = [[1, 2, 3], [0], [0], [0]]
+    print(s.shortestPathLength(graph1))  # Output: 4
+
+    graph2 = [[1], [0, 2, 4], [1, 3, 4], [2], [1, 2]]
+    print(s.shortestPathLength(graph2))  # Output: 4
+
+if __name__ == '__main__':
+    main()
