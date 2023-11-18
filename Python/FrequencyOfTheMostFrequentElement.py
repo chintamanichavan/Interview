45b3586ac6fc004081c1f46d6b70a183b157ceb4
Chintamani Chavan <55659337+chintamanichavan@users.noreply.github.com>
2023-11-17 19:56:58 -0600

    Create FrequencyOfTheMostFrequentElement.py

---
+++ Python/FrequencyOfTheMostFrequentElement.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        left = 0
+        total = 0
+        max_freq = 0
+
+        for right in range(len(nums)):
+            total += nums[right]
+
+            # Check if the total sum of differences between nums[right] and each element in the window
+            # is greater than k. If it is, move the left pointer to the right.
+            while nums[right] * (right - left + 1) > total + k:
+                total -= nums[left]
+                left += 1
+
+            max_freq = max(max_freq, right - left + 1)
+
+        return max_freq
