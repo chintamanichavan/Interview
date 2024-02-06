22b20d11e0923fd93c8bf83094dbdd0ea15557c2
Chintamani Chavan <55659337+chintamanichavan@users.noreply.github.com>
2024-02-06 06:32:49 -0600

    Update GroupAnagrams.py

--- Python/GroupAnagrams.py
+++ Python/GroupAnagrams.py
@@ -1,25 +1,15 @@
-from typing import List
 from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
 
+        anagrams = {}
         for s in strs:
-            count = [0] * 26
-
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-
-            res[tuple(count)].append(s)
-
-        print(res.values())
-        return res.values()
-
-def main():
-    s = Solution()
-    strs = ["eat","tea","tan","ate","nat","bat"]
-    s.groupAnagrams(strs)
-
-if __name__ == '__main__':
-    main()
+            # Sort the string to use as a key
+            key = tuple(sorted(s))
+            if key not in anagrams:
+                anagrams[key] = []
+            anagrams[key].append(s)
+        
+        # Return grouped anagrams
+        return list(anagrams.values())
