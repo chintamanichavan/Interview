c998a67670a4608ec24c4d8b5cd7107277775ce9
chintamanichavan <chintamani.chavan08@gmail.com>
2024-04-09 22:49:06 -0500

    added leetcode solution 20240409

---
+++ Python/RevealCardsInIncreasingOrder.py
@@ -0,0 +1,13 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        n = len(deck)
+        index = list(range(n))
+        result = [0] * n
+
+        for card in sorted(deck):
+            result[index.pop(0)] = card
+            if index:
+                index.append(index.pop(0))
+
+        return result:wq
+
