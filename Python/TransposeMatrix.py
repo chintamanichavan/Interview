3fe7a5978a4b58e3259ba0375d9d7d28b263abda
chintamanichavan <chintamani.chavan08@gmail.com>
2024-08-31 23:35:00 -0500

    annotate: TransposeMatrix

--- Python/TransposeMatrix.py
+++ Python/TransposeMatrix.py
@@ -2,3 +2,5 @@ class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
         # Transpose the matrix
         return [list(row) for row in zip(*matrix)]
+
+# review 2024-08-31
