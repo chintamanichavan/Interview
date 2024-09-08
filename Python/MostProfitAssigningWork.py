f3de43d955df3d9d95b86a5a7b19e58174d84c28
chintamanichavan <chintamani.chavan08@gmail.com>
2024-09-08 07:18:00 -0500

    refactor: MostProfitAssigningWork

--- Python/MostProfitAssigningWork.py
+++ Python/MostProfitAssigningWork.py
@@ -28,3 +28,5 @@ class Solution:
 sol = Solution()
 print(sol.maxProfitAssignment([2, 4, 6, 8, 10], [10, 20, 30, 40, 50], [4, 5, 6, 7]))  # Output: 100
 print(sol.maxProfitAssignment([85, 47, 57], [24, 66, 99], [40, 25, 25]))  # Output: 0
+
+# review 2024-09-08
